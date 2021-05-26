56cede44bca5fd0edd04f4db4fe3f0d38e8b125f
fasikaWalle <fasikabini12@gmail.com>
2021-05-25 21:09:21 -0500

    schema

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Tab from "./components/Tab.js";
 function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <>
+        <Switch>
+          <Route path="/" exact component={Tab} />
+        </Switch>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Tab.js/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "antd/dist/antd.css";
+
+import { Tabs, PageHeader, Layout } from "antd";
+import Login from "../Login";
+import Signup from "../Signup";
+import "../../styles/tab.css";
+const { TabPane } = Tabs;
+const { Content } = Layout;
+
+const Tab = () => (
+  // <Layout>
+  //   <Content>
+  <div className="tab-container" breakpoint="xs">
+    <PageHeader className="site-page-header" title="Zing Chat" />
+    <Tabs defaultActiveKey="1" className="form-tab">
+      <TabPane tab="Login" key="1">
+        <Login />
+      </TabPane>
+      <TabPane tab="Sign up" key="2">
+        <Signup />
+      </TabPane>
+    </Tabs>
+  </div>
+  //   </Content>
+  // </Layout>
+);
+
+export default Tab;
