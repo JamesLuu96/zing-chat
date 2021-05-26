57268f8aefe6d97ecd7dd681f00fb88312c8bf28
JamesLuu96 <32526792+JamesLuu96@users.noreply.github.com>
2021-05-25 21:02:35 -0500

    initial sockets

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, { cors: {origin:'*'}})
 
+io.on('connection', function(socket){
+  socket.on("send message", function(message){
+    io.emit("receive message", message)
+  })
+})
+
+http.listen(4000, function(){
+  console.log('listening on port 4000')
+})
