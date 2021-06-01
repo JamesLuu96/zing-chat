191c72c456b8d54260929aa177e286ee8502c5e6
fasikaWalle <fasikabini12@gmail.com>
2021-06-01 01:54:10 -0500

    delete rom

--- client/src/components/RoomList/RoomCard/index.js
+++ client/src/components/RoomList/RoomCard/index.js
@@ -2,45 +2,66 @@ import React from "react";
 import { Card, Avatar, Button, Col, Row, Tag } from "antd";
 import AvatarContact from "react-avatar";
 import { Link } from "react-router-dom";
-
+import { DeleteOutlined } from "@ant-design/icons";
 export default function RoomCard({ room, setFilterString }) {
-  const {users, category, roomName} = room
-  const id = room._id
-
-  function filterListByTag(tagName){
-	setFilterString(tagName)
+  const { users, category, roomName, username } = room;
+  const id = room._id;
+  console.log(room);
+  function filterListByTag(tagName) {
+    setFilterString(tagName);
   }
-	return (
-		<Card title={roomName} extra={
-      <Link to={{pathname: `/room/${id}`, state: {roomName: room.roomName, roomId: id} }}><Button>Join &rarr;</Button></Link>
-    }>
-			<Row justify="space-between">
-				<Col>
-					{category.map((tag, i) => {
-						return (
-							<Tag color="magenta" className="filterTags" onClick={e=>filterListByTag(tag)} key={i}>
-								{tag}
-							</Tag>
-						);
-					})}
-				</Col>
-				<Col>
-				<Avatar.Group
-					maxCount={5}>
-					{users.map((user, i) => {
-						return (
-								<AvatarContact
-									key={i}
-									className="avatar-round"
-									size="32"
-									round={true}
-									name={user}
-								/>
-								)
-							})}
-					</Avatar.Group>
-				</Col>
-			</Row>
-		</Card>
-	);
+  const deleteHandler = () => {};
+  return (
+    <Card
+      title={roomName}
+      extra={
+        <Link
+          to={{
+            pathname: `/room/${id}`,
+            state: { roomName: room.roomName, roomId: id },
+          }}
+        >
+          {room.username ? (
+            <Button style={{ marginRight: "1rem" }} onClick={deleteHandler}>
+              Delete <DeleteOutlined />
+            </Button>
+          ) : null}
+
+          <Button>Join &rarr;</Button>
+        </Link>
+      }
+    >
+      <Row justify="space-between">
+        <Col>
+          {category.map((tag, i) => {
+            return (
+              <Tag
+                color="magenta"
+                className="filterTags"
+                onClick={(e) => filterListByTag(tag)}
+                key={i}
+              >
+                {tag}
+              </Tag>
+            );
+          })}
+        </Col>
+        <Col>
+          <Avatar.Group maxCount={5}>
+            {users.map((user, i) => {
+              return (
+                <AvatarContact
+                  key={i}
+                  className="avatar-round"
+                  size="32"
+                  round={true}
+                  name={user}
+                />
+              );
+            })}
+          </Avatar.Group>
+        </Col>
+      </Row>
+    </Card>
+  );
 }
