e6c3832194530a1873f90339b3f294659cb645d0
flokamp <74511633+flokamp@users.noreply.github.com>
2021-06-02 01:30:43 -0500

    header stuffs

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -19,22 +19,6 @@ import { useLocation } from "react-router-dom";
 
 const { Content } = Layout;
 
-// const Editor = ({ handleEditorChange, submitting, value }) => (
-// 	<>
-// 		<Form.Item className="text-editor">
-// 			<TextEditor onChange={handleEditorChange} value={value} />
-// 		</Form.Item>
-// 		<Form.Item>
-// 			<Button
-// 				icon={<SendOutlined />}
-// 				htmlType="submit"
-// 				loading={submitting}
-// 				onClick={submitForm}
-// 				type="primary"></Button>
-// 		</Form.Item>
-// 	</>
-// );
-
 export default function Chat({ handleChange }) {
 	const location = useLocation();
 	const { roomName, roomId } = location.state;
