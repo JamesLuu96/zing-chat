7b58795e57df36d6846ace316dd303e695a5f16c
flokamp <74511633+flokamp@users.noreply.github.com>
2021-06-02 18:37:15 -0500

    added less file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import useLocalStorage from "./hooks/useLocalStorage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+import "./App.less";
 import Nav from "./components/Nav";
 import Main from "./pages/Main";
 import Socket from "./components/Socket";
@@ -15,37 +15,37 @@ import ApolloClient from "apollo-boost";
 import Auth from "./utils/auth";
 
 export default function App() {
-  const [idToken, setIdToken] = useLocalStorage("id_token");
-  const client = new ApolloClient({
-    request: (operation) => {
-      operation.setContext({
-        headers: {
-          authorization: idToken ? `Bearer ${idToken}` : "",
-        },
-      });
-    },
-    uri: "/graphql",
-  });
-  const dashboard = (
-    <ApolloProvider client={client}>
-      <Router>
-        <Socket idToken={idToken}>
-          <Nav idToken={idToken} />
-          <Switch>
-            <Route exact path="/error" component={Error} />
-            <Route exact path="/" component={Main} />
-            <Route exact path="/room/:id" component={Chat} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Socket>
-      </Router>
-    </ApolloProvider>
-  );
-  return idToken && !Auth.isTokenExpired(idToken) ? (
-    dashboard
-  ) : (
-    <ApolloProvider client={client}>
-      <Authentication setIdToken={setIdToken} />
-    </ApolloProvider>
-  );
+	const [idToken, setIdToken] = useLocalStorage("id_token");
+	const client = new ApolloClient({
+		request: (operation) => {
+			operation.setContext({
+				headers: {
+					authorization: idToken ? `Bearer ${idToken}` : "",
+				},
+			});
+		},
+		uri: "/graphql",
+	});
+	const dashboard = (
+		<ApolloProvider client={client}>
+			<Router>
+				<Socket idToken={idToken}>
+					<Nav idToken={idToken} />
+					<Switch>
+						<Route exact path="/error" component={Error} />
+						<Route exact path="/" component={Main} />
+						<Route exact path="/room/:id" component={Chat} />
+						<Route component={NoMatch} />
+					</Switch>
+				</Socket>
+			</Router>
+		</ApolloProvider>
+	);
+	return idToken && !Auth.isTokenExpired(idToken) ? (
+		dashboard
+	) : (
+		<ApolloProvider client={client}>
+			<Authentication setIdToken={setIdToken} />
+		</ApolloProvider>
+	);
 }
