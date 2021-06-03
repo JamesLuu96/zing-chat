7c2de1c9283f5a0a7dea5069aed5da1d7529c756
JamesLuu96 <32526792+JamesLuu96@users.noreply.github.com>
2021-06-02 19:03:05 -0500

    join room

--- client/src/components/UserList/UserCard/index.js
+++ client/src/components/UserList/UserCard/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { List, Button, Badge, Avatar } from "antd";
+import { List, Button, Badge, Avatar, Tooltip } from "antd";
 import { UserAddOutlined } from "@ant-design/icons";
 import { useMutation } from "@apollo/react-hooks";
 import { ADD_FRIEND } from "../../../utils/mutations";
 import { useMyInfo, useSocket } from "../../Socket";
+import {Link} from "react-router-dom"
+import { RightSquareOutlined } from "@ant-design/icons";
 
 export default function UserCard({ user, friends, setFriends }) {
   const [addFriend, { error }] = useMutation(ADD_FRIEND);
@@ -42,8 +44,15 @@ export default function UserCard({ user, friends, setFriends }) {
 
         {friends.filter((friend) => friend._id === user.id) < 1 &&
         user.id !== userData._id ? (
-          <Button onClick={addFriendHandler} icon={<UserAddOutlined />} />
+          <Tooltip title={`Add ${user.username}?`}><Button onClick={addFriendHandler} icon={<UserAddOutlined />} /></Tooltip>
         ) : null}
+        {user.room !== "Lobby" ? 
+        <Tooltip title={`Join ${user.username}'s room?`}>
+          <Link to={{pathname: `/room/${user.room}`, state: {roomName: user.roomName, roomId: user.room} }}>
+            <Button icon={<RightSquareOutlined />}/> 
+			    </Link>
+        </Tooltip>
+        : null}
       </List.Item>
     </>
   );
